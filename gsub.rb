796a3a3961ae5c5e1e9ad99f2bcb62f4772f3b90
Rhyd Lewis <rhyd.lewis@icloud.com>
2022-12-01 07:33:23 +0000

    minor updates

--- gsub.rb
+++ gsub.rb
@@ -1,19 +1,15 @@
 #! /usr/bin/env ruby
-
 query = ARGV
 pbdata = `pbpaste`
-
 pattern = ""
 replacement = ""
-
 pattern = query[0]
 
 if query.size > 1
-	replacement = query[1]
+  replacement = query[1]
 else
-	replacement = ""
+  replacement = ""
 end
 
 result = pbdata.gsub(pattern, replacement)
-
-`echo #{result} | pbcopy`
+puts(result)
